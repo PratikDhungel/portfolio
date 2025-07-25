7c16a9c3b69dcb87b119b2b3eab32ab765ede5f7
PratikDhungel <pratikdhungel7.0@gmail.com>
2025-07-25 16:02:42 +0545

    Portfolio: Create work experience section for recent work

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,9 +1,33 @@
 const WorkExperience = () => {
   return (
     <div>
-      <p className="text-2xl mb-4">
-        <strong className="border-b-2 pb-2">Work Experience</strong>
-      </p>
+      <div>
+        <p className="text-2xl mb-8">
+          <strong className="border-b-2 pb-2">Work Experience</strong>
+        </p>
+      </div>
+
+      <div>
+        <div>
+          <p className="text-xl mb-1">
+            <strong>Frontend Engineer</strong>
+          </p>
+          <p className="text-xl">
+            <strong>Returnalyze</strong>
+          </p>
+
+          <p className="text-sm mb-2">(Feb 2024 - Jan 2025)</p>
+
+          <div>
+            <ul className="list-disc list-inside ml-1">
+              <li className="text-sm">
+                Designed interactive charts using React (with React Highcharts) for a returns analytics platform which
+                empowers brands to reduce return rates, lower operational costs and uncover hidden revenue opportunities
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
